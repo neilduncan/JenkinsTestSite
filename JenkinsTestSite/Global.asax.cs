ff43950ff2f761cba2a4dcb6696060f216716514
neilduncan <neil@aqueduct.co.uk>
2012-02-03 15:47:33 +0000

    make it work as a website

--- JenkinsTestSite/Global.asax.cs
+++ JenkinsTestSite/Global.asax.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace JenkinsTestSite
@@ -24,9 +20,8 @@ namespace JenkinsTestSite
             routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+                new { controller = "Test", action = "Index", id = UrlParameter.Optional } // Parameter defaults
             );
-
         }
 
         protected void Application_Start()
