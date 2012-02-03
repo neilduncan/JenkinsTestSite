e3cb391bc40659a4f5e459a370dd034bf3579f89
neilduncan <neil@aqueduct.co.uk>
2012-02-03 12:17:42 +0000

    add some tests

---
+++ Example.Tests/TestClass.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Example.Tests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void Passing_Test()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        public void Failing_Test()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
